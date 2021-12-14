659ec900aa07f6803cfd901010f7762b1011e631
Victor Tardif <victor.tardif1@gmail.com>
2021-12-14 10:46:15 +0100

    js recette done

--- assets/server/serverRecette.mjs
+++ assets/server/serverRecette.mjs
@@ -4,7 +4,7 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 
-import { getRecettes, addRecette } from "./index.mjs";
+import { getRecettes, addRecette } from "./functionsRecette.mjs";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
